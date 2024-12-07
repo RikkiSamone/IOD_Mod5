fca44ddba9acc4283adcebce4eef9896954f5401
Rikki <rikkisamone@gmail.com>
2024-12-07 11:56:53 -0800

    lab 1: complete

--- myapp/index.js
+++ myapp/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = [3000, 4001];
 
 app.get('/', (req, res) => {
 res.send('Hello World!')
 })
 
-app.listen(port, () => {
-console.log(`Example app listening
-at http://localhost:${port}`)
-})
+port.forEach(port => {
+    app.listen(port, () => {
+        console.log(`Example app listening
+at http://localhost:${port}`);
+    });
+});
